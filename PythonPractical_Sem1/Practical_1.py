cbcb07631aea8c740aabb4a6ecf4db9edad5c8bc
Mohsin Sayed <mohsinsayed1999@gmail.com>
2022-11-11 23:47:43 +0530

    Python Practical 1 and 2

---
+++ PythonPractical_Sem1/Practical_1.py
@@ -0,0 +1,25 @@
+# prints the string
+print('Hello World! I am Python')
+
+# declaring and printing integer type variable
+a = 1000
+print(a)
+
+# declaring integer, string, boolean and float type variable
+a = 999
+b = "Mohsin's name is a string"
+c = True
+d = 10.01
+
+# printing variable type using type() method
+print(type(a),type(b),type(c),type(d))
+
+# concatenation within print method
+print(999+1)
+print(a+d)
+
+# this is a single line comment
+'''
+this is a multiline
+comment
+'''
---
+++ PythonPractical_Sem1/Practical_2.py
@@ -0,0 +1,25 @@
+# Conditional Statements in Python
+a = 69
+b = 420
+if a > b:
+    print("'a' is greater than 'b'")
+else:
+    print("'b' is greater than 'a'")
+
+# Looping/Recursion in Python
+for i in range(5):
+    print(i)
+
+l = ['a','b','c','d','e']
+for i in l:
+    print(l)
+
+while a > 65:
+    print('looping')
+    a -= 1
+
+# Functions in Python
+def addNumbers(x,y):
+    print(x+y)
+
+addNumbers(69,420)
